4492c7d8b777f2e3b7b5765e33d6f89612bbc194
Bastien Kloepfer <bastien.kloepfer@numeric-wave.eu>
2025-02-26 14:38:53 +0100

    [NIL] Start to add CoreBundle for doctrine naming strategy

---
+++ src/Lucca/Bundle/CoreBundle/LuccaCoreBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright (c) 2025. Numeric Wave
+ *
+ * Afero General Public License (AGPL) v3
+ *
+ * For more information, please refer to the LICENSE file at the root of the project.
+ */
+
+namespace Lucca\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LuccaCoreBundle extends Bundle
+{
+}
