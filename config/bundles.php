fef2ae48cae33533f8c8af7e861d7e2d4a1145af
Bastien Kloepfer <bastien.kloepfer@numeric-wave.eu>
2025-03-03 10:39:49 +0100

    [#8] Add checklistBundle, add missing routes in config

--- config/bundles.php
+++ config/bundles.php
@@ -8,11 +8,16 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+
+    /** Extra Bundles */
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+
+    /** Lucca Bundles */
     Lucca\Bundle\AdherentBundle\LuccaAdherentBundle::class => ['all' => true],
     Lucca\Bundle\CoreBundle\LuccaCoreBundle::class => ['all' => true],
     Lucca\Bundle\LogBundle\LuccaLogBundle::class => ['all' => true],
@@ -22,5 +27,4 @@ return [
     Lucca\Bundle\ThemeDocsUiKitBundle\LuccaThemeDocsUiKitBundle::class => ['all' => true],
     Lucca\Bundle\ThemeStreamDashboardUiKitBundle\LuccaThemeStreamDashboardUiKitBundle::class => ['all' => true],
     Lucca\Bundle\UserBundle\LuccaUserBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
---
+++ src/Lucca/Bundle/ChecklistBundle/DataFixtures/ORM/ChecklistFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Copyright (c) 2025. Numeric Wave
+ *
+ * Afero General Public License (AGPL) v3
+ *
+ * For more information, please refer to the LICENSE file at the root of the project.
+ */
+
+namespace Lucca\Bundle\ChecklistBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+
+use Lucca\Bundle\ChecklistBundle\Entity\Checklist;
+
+class ChecklistFixtures extends AbstractFixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $checkLists = [
+            ['name' => 'Liste PV', 'status' => 'choice.status.minute', 'ref' => 'checklist-1'],
+            ['name' => 'Liste Actualisation', 'status' => 'choice.status.updating', 'ref' => 'checklist-2'],
+        ];
+
+        foreach ($checkLists as $checklist) {
+
+            $newChecklist = new Checklist();
+            $newChecklist->setName($checklist['name']);
+            $newChecklist->setStatus($checklist['status']);
+            $newChecklist->setEnabled(true);
+
+            $manager->persist($newChecklist);
+            $this->addReference($checklist['ref'], $newChecklist);
+        }
+
+        $manager->flush();
+    }
+}
