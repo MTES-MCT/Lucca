628d9bbc2504dc46fa8308e3631bc4dfda8eac6c
Alizee Meyer <alizee.m@numeric-wave.eu>
2025-12-09 18:22:42 +0100

    [NIL] Add tribunalCompetentAddress key in print courrier + add clash message to display API geolocator error + Change version to v4.3.1

--- src/Lucca/Bundle/CoreBundle/Service/GeoLocator.php
+++ src/Lucca/Bundle/CoreBundle/Service/GeoLocator.php
@@ -198,7 +198,9 @@ class GeoLocator
         $googleResultsDecoded = json_decode($response?->getContent(false) ?? '');
 
         if (!$response || $googleResultsDecoded->status !== 'OK') {
-            $this->requestStack->getSession()->getFlashBag()->add('danger', 'Les résultats renvoyés par l\'API Google ont été rejetés - ' . $response?->getStatusCode() ?? 'No Status code');
+            $this->requestStack->getSession()->getFlashBag()->add('danger', 'Les résultats renvoyés par l\'API Google ont été rejetés - ' . $response?->getStatusCode() ?? 'No Status code' );
+            if($googleResultsDecoded->error_message)
+                $this->requestStack->getSession()->getFlashBag()->add('danger', 'Erreur technique : ' . $googleResultsDecoded->error_message);
 
             $this->logger->error('Google Maps API error for Geocode: ' . $googleResultsDecoded?->status ?? 'No Status' . ' for address: ' . $googleGeoLink . ' - error: ' . $googleResultsDecoded?->error_message ?? 'No error message provided');
             return [];
