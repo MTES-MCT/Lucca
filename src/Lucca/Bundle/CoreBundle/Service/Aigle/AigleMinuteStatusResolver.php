52ef8f292de5c1c7c24d46bcafb93ed00ed07bd2
Arthur Déléage <arthur.deleage@numeric-wave.eu>
2026-01-12 15:53:31 +0100

    [NIL] Aigle API - Update status resolve to remove UPDATING and CLOSURE status when status are sync.

--- src/Lucca/Bundle/CoreBundle/Service/Aigle/AigleMinuteStatusResolver.php
+++ src/Lucca/Bundle/CoreBundle/Service/Aigle/AigleMinuteStatusResolver.php
@@ -15,35 +15,22 @@ use Lucca\Bundle\MinuteBundle\Entity\Minute;
 
 readonly class AigleMinuteStatusResolver
 {
-
-//    const STATUS_OPEN = 'open';
-//    const STATUS_CONTROL = 'control';
-//
-//    const STATUS_FOLDER = 'folder';
-//    const STATUS_REPORT = 'report';
-//    const STATUS_AIT = 'ait';
-//    const STATUS_UPDATING = 'updating';
-//    const STATUS_CLOSURE = 'closure';
-//    const STATUS_REGULARISED = 'regularised';
-
-
     //not match in lucca
-    const STATUS_PRIOR_LETTER_SENT = 'PRIOR_LETTER_SENT'; // TODO: Ne pas utiliser quand le status est STATUS_COURIER ??
-    const STATUS_ADMINISTRATIVE_CONSTRAINT = 'ADMINISTRATIVE_CONSTRAINT';
+    const STATUS_ADMINISTRATIVE_CONSTRAINT = 'ADMINISTRATIVE_CONSTRAINT'; //Not implemented on lucca only on aigle
 
     //implemented and match in lucca
     const STATUS_NOT_CONTROLLED = 'NOT_CONTROLLED';
     const STATUS_CONTROLLED_FIELD = 'CONTROLLED_FIELD';
-
     const STATUS_OFFICIAL_REPORT_DRAWN_UP = 'OFFICIAL_REPORT_DRAWN_UP';
     const STATUS_OBSERVARTION_REPORT_REDACTED = 'OBSERVARTION_REPORT_REDACTED';
     const STATUS_REHABILITATED = 'REHABILITATED';
+    const STATUS_PRIOR_LETTER_SENT = 'PRIOR_LETTER_SENT';
 
-    //not use in aigle but anyway sent
+    //not implemented in aigle
     const STATUS_UPDATING = 'updating';
     const STATUS_CLOSURE = 'closure';
 
-    //not use in aigle
+    //not use implemented on lucca and aigle
     const STATUS_AIT = 'ait';
 
     public function statusResolver(Minute $minute): ?string
@@ -52,29 +39,30 @@ readonly class AigleMinuteStatusResolver
             Minute::STATUS_OPEN => self::STATUS_NOT_CONTROLLED,
             Minute::STATUS_CONTROL => self::STATUS_CONTROLLED_FIELD,
             Minute::STATUS_FOLDER => self::STATUS_OFFICIAL_REPORT_DRAWN_UP,
-//            Minute::STATUS_COURIER => self::STATUS_REPORT, //TODO: a check car pas de status report
+//            Minute::STATUS_COURIER => null, //Not used in Aigle
+//            Minute::STATUS_AIT => null, //Not used in Aigle
             Minute::STATUS_UPDATING => $this->updatingStatusResolver($minute),
+            //Minute::STATUS_DECISION => null, //Not used anywhere
             Minute::STATUS_CLOSURE => $this->clotureStatusResolver($minute),
-            //Minute::STATUS_DECISION => self::STATUS_REGULARISED, -> TODO: a check car pas de status regularised dans lucca pour Minute
             default => null,
         };
     }
 
-    private function clotureStatusResolver(Minute $minute): string
+    private function clotureStatusResolver(Minute $minute): ?string
     {
 
         if ($minute->getClosure()?->getStatus() === Closure::STATUS_REGULARIZED) {
             return self::STATUS_REHABILITATED;
         }
 
-        return self::STATUS_CLOSURE;
+//        return self::STATUS_CLOSURE;
+        return null; //Return null because CLOSURE status is not used in Aigle
     }
 
     // Determine if the status should be OBSERVARTION_REPORT_REDACTED or UPDATING
     // based on the presence of Natinfs in the last folder's controls
     // If there are no Natinfs, return OBSERVARTION_REPORT_REDACTED
-    // Otherwise, return UPDATING
-    private function updatingStatusResolver(Minute $minute): string
+    private function updatingStatusResolver(Minute $minute): ?string
     {
         $lastFolder = null;
         /** @var Control $control */
@@ -103,6 +91,7 @@ readonly class AigleMinuteStatusResolver
             return self::STATUS_OBSERVARTION_REPORT_REDACTED;
         }
 
-        return self::STATUS_UPDATING;
+//        return self::STATUS_UPDATING;
+        return null; //Return null because UPDATING status is not used in Aigle
     }
 }
