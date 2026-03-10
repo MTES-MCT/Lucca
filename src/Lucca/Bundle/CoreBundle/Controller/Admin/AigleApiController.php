e61fcfb4c16b889e937b199c2cc0d0444155ff12
Arthur Déléage <arthur.deleage@numeric-wave.eu>
2026-03-10 18:36:57 +0100

    [NIL] - AigleApi - Add API configuration check and flash message for unset API.

--- src/Lucca/Bundle/CoreBundle/Controller/Admin/AigleApiController.php
+++ src/Lucca/Bundle/CoreBundle/Controller/Admin/AigleApiController.php
@@ -13,19 +13,32 @@ namespace Lucca\Bundle\CoreBundle\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController,
     Symfony\Component\HttpFoundation\Response,
     Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 use Lucca\Bundle\SecurityBundle\Form\BrowserLoginAttemptType;
+use Lucca\Bundle\CoreBundle\Service\Aigle\AigleApiClient;
 
 #[Route(path: '/admin/aigle-api')]
 #[IsGranted('ROLE_ADMIN')]
 class AigleApiController extends AbstractController
 {
+    public function __construct(
+        private readonly AigleApiClient $aigleApiClient,
+    ) {}
 
     #[Route(path: '/', name: 'lucca_core_admin_aigle_api_index', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function indexAction(): Response
+    public function indexAction(Request $request): Response
     {
+        if (!$this->aigleApiClient->apiAreSet()) {
+            $referer = $request->headers->get('referer');
+            $this->addFlash('danger', 'flash.aigleApi.apiNotSet');
+            if ($referer) {
+                return $this->redirect($referer);
+            }
+            return $this->redirectToRoute('lucca_core_parameter');
+        }
         $form = $this->createForm(BrowserLoginAttemptType::class);
 
         return $this->render('@LuccaCore/Admin/aigleApi.html.twig', [
